1146f578e7a12deae2bc3cbac85af1c60dea1d5f
deniskeller <dentean@mail.ru>
2020-08-21 18:39:52 +0400

    refactoring

---
+++ src/model.js
@@ -0,0 +1,31 @@
+import image from "./assets/image.png";
+import {
+  TitleBlock,
+  ImageBlock,
+  TextBlock,
+  TextColumsBlock,
+} from "./classes/blocks";
+
+export const model = [
+  new TitleBlock("Конструктор сайтов на чистом Javascript", {
+    tag: "h2",
+    styles: "background: darkred; color: #fff; text-align: center;",
+  }),
+
+  new ImageBlock(image, {
+    styles: "padding: 2rem 0; display: flex; justify-content: center;",
+    alt: "my image",
+    imageStyles: "width: 500px; height: auto;",
+  }),
+
+  new TextBlock(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quo.",
+    {
+      styles: "background: darkblue; color: yellow;",
+    }
+  ),
+
+  new TextColumsBlock(["1 text", "2 text", "3 text", "2 text", "3 text"], {
+    styles: "padding: 1rem;",
+  }),
+];
